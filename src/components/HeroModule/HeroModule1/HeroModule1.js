326c53969c720b6ed2eb1383a05111182381d39e
Thiruselvam11 <muruganselva986@gmail.com>
2023-03-02 12:43:01 +0530

    second commit

--- src/components/HeroModule/HeroModule1/HeroModule1.js
+++ src/components/HeroModule/HeroModule1/HeroModule1.js
@@ -8,17 +8,18 @@ const HeroModule1 = (props) => {
         imgSrc
     } = props
     return (
-        <section>
-            <div className='flex flex-row'>
-                <div className='w-2/4 flex justify-center flex-col'>
-                    <h1>Timeless and comfy</h1>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+            <div class='w-full h-full flex flex-col-reverse bg-[#e9e9e9] md:flex-row justify-center'>
+                <div class='w-full flex justify-center flex-col px-3 py-2 self-center flex-wrap sm:w-full md:w-2/4'>
+                    <h1 class="w-full font-bold text-left py-5">Timeless and comfy</h1>
+                    <p class="w-full text-left font-normal"> A symphony of beautiful earthy shades with an open toe,
+                        Maroon Studded Heels made of PU leather are sustainable, super comfortable and super stylish.</p>
                 </div>
-                <div className='w-2/4 justify-center flex-col'>
+                <div class='w-full bg-black justify-center flex-col sm:w-full md:w-2/4'>
                     <img src="https://syndicationv2.weaveroo.io/cai-01/en/maroon-studded-heels/1/image/1c38cfab-1810-464c-8d2a-95d2b08110fb.webp" />
                 </div>
             </div>
-        </section>
+
+
         // <div className='w-full bg-white py-16 px-4'>
         //     <div className='max-w-[1240px] mx-auto grid md:grid-cols-2'>
         //         <img className='w-[500px] mx-auto my-4' src={imgSrc} alt='/' />
